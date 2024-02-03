fe78e38a9eea97c14a67d7467c8e794e926fcf5f
pjadhav1234 <147049657+pjadhav1234@users.noreply.github.com>
2024-02-03 11:45:00 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.text', {
+  strings: ['frontend developer', 'DSA learner', 'C++ developer'],
+  typeSpeed: 50,
+  backSpeed: 70,
+  backDelay: 100
+});
+
+/*var typed = new Typed('#element', {
+    strings: ['<i>begginer</i> of dsa.', '&amp;dsa in c++'],
+    typeSpeed: 50,
+  });
+</script>*/
